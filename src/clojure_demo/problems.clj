3b0d05b71b59322f3e53d80e9f7909df97a19c15
Leo Wei <vn55nz6@homeoffice.wal-mart.com>
2023-04-28 17:47:59 -0400

    Add problem.clj including Palindrome problem

---
+++ src/clojure_demo/problems.clj
@@ -0,0 +1,97 @@
+(ns clojure-demo.problems
+  (:gen-class))
+
+
+;; Palindrome
+;; write a function to find out if string is a palindrome
+;(first string)
+;(last string)
+;(rest string)
+;(take (- (count string) 1) string)
+
+(defn palindrome [string]
+  (if
+    (or (= (count string) 0) (= (count string) 1))
+    true
+    (if
+      (= (first string) (last string))
+      (palindrome (rest
+                    (take
+                      (- (count string) 1)
+                      string)))
+      false)))
+
+(defn palindrome [string]
+  (= (seq string) (reverse string)))
+
+(def string-palin (str "abcba"))
+(def string-not-palin (str "acnjdson"))
+
+
+
+
+
+;; find the number of 'c's in string
+(def string "abccasc")
+(defn find-c [string]
+  (count
+    (filter (fn [char]
+              (= char \c))
+            (seq string))))
+
+
+
+
+
+;; my-filter
+(defn my-filter [f, coll]
+  (reduce (fn [output ele]
+            (if (f ele)
+              (conj output ele)
+              output))
+          []
+          coll))
+
+(defn find-c-my-filter [string]
+  (count
+    (my-filter (fn [char]
+                 (= char \c))
+               (seq string))))
+
+;; we can not use map to implement my-filter
+;; map can not change the size
+;; the output variable is immutable
+(defn my-filter [f coll]
+  (let [output []]
+    (map (fn [x]
+           (if (f x)
+             (conj output x)))
+         coll)
+    output))
+
+
+;; find the first 100 prime number
+
+
+
+
+
+;; Rich Comment Blocks
+(comment
+
+  ;; palindrome
+  (palindrome string-palin)
+  (palindrome string-not-palin)
+
+  ;; count c
+  (find-c string)
+
+  ;; how to delete the outer brackets
+  [(seq string)]
+
+  ;; my-filter
+  (find-c-my-filter string)
+
+  (let [n 10]
+    (not-any? f '(1 2 3)))
+  )
